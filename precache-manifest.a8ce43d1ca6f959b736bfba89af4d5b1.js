80e83e741aacb68761962a665591042c2c3a94ba
Santiago <san.or.gue@gmail.com>
2019-05-31 02:19:27 -0500

    Updates

---
+++ precache-manifest.a8ce43d1ca6f959b736bfba89af4d5b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9791db56bb6d1abc034f",
+    "url": "/alter-app/static/js/runtime~main.91867c51.js"
+  },
+  {
+    "revision": "73202ab174ed68938e5b",
+    "url": "/alter-app/static/js/main.12fb8eeb.chunk.js"
+  },
+  {
+    "revision": "2eca8472fb95fee3fc31",
+    "url": "/alter-app/static/js/2.d16f1d23.chunk.js"
+  },
+  {
+    "revision": "73202ab174ed68938e5b",
+    "url": "/alter-app/static/css/main.7647f4c0.chunk.css"
+  },
+  {
+    "revision": "2eca8472fb95fee3fc31",
+    "url": "/alter-app/static/css/2.a4260f37.chunk.css"
+  },
+  {
+    "revision": "e896200e895924f8e0a492db13440f2f",
+    "url": "/alter-app/index.html"
+  }
+];
